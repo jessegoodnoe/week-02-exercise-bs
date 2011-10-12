d8fb747728979186360aa71fc6a1f3fc20072626
Jesse Goodnoe <jessegoodnoe@me.com>
2011-10-11 22:50:47 -0700

    Fixed unit test.

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -3,7 +3,7 @@ describe "Strings" do
 
     it "should remove all whitespace from the beginning and the end of the string" do
       lyrics = "  Hello, is it me you're looking for I can see it in your eyes  "
-      lyrics.strip.should eq "Hello, is it me you're looking for I can see it in your eyes "
+      lyrics.strip.should eq "Hello, is it me you're looking for I can see it in your eyes"
     end
   end
 end
